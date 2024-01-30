ef70deb0a361c23929980ae04f0292c01b05407c
Ty Jacalan <muzuuu@protonmail.com>
2024-01-30 19:56:46 +0800

    chore: add delete sample

--- postgres/intro.sql
+++ postgres/intro.sql
@@ -15,8 +15,23 @@ VALUES(1, 'Juan', 'Cruz', 18,'Manila'),
        (5, 'Zack', 'Matthews', 22,'Marikina'),
        (6, 'Finn', 'Lam', 25, 'Manila');
 
+--Update the first record
 UPDATE students
 SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan'
 WHERE id = 1;
 
+--Delete the last record
+DELETE FROM students WHERE id = (SELECT COUNT (*) FROM students);
+
+--Display the count of all students
+SELECT COUNT(*) FROM students;
+
+--Select all students with location is Manila
+SELECT * FROM students WHERE location = 'Manila';
+
+--Display the average age of all students
+SELECT AVG(age) FROM students;
+
+--Display all students by age descending order
+SELECT * FROM students ORDER BY age DESC;
 
