876fbe196c88f0d9f2981d353e60035a8788428d
Ty Jac <muzuuu@protonmail.com>
2024-02-20 20:17:32 +0800

    refactor: convert loop to map operation

--- ruby/12_the_vowel_code.rb
+++ ruby/12_the_vowel_code.rb
@@ -1,31 +1,22 @@
+VOWELS = {
+    'a' => 1,
+    'e' => 2,
+    'i' => 3,
+    'o' => 4,
+    'u' => 5
+}
+
 def encode(str)
-    vowel_pairs = {'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5}
-    encoded_str = ""
-
-    str.each_char do |char|
-        if vowel_pairs[char]
-            char = vowel_pairs[char].to_s
-        end
-
-        encoded_str += char
-    end
-
-    encoded_str
+    str.chars.map do |char|
+        VOWELS[char] || char
+    end.join
 end
 
 def decode(str)
 
-    number_pairs = {1 => 'a', 2 => 'e', 3 => 'i', 4 => 'o', 5 => 'u'}
-    decoded_str = ""
-
-    str.each_char do |char|
-        if number_pairs[char.to_i]
-            char = number_pairs[char.to_i]
-        end
-        
-        decoded_str += char
-    end
-    decoded_str
+    str.chars.map do |char|
+        VOWELS.key(char.to_i) || char
+    end.join
 end
 
 encoded = encode("hi there")
